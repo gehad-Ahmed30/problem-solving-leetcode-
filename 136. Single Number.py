928bd18212c0363a62e9c2cafa224b84dd33b7ea
gehad-ahme <gaded3082003@gmail.com>
2024-08-15 16:48:39 +0300

    peoject

---
+++ 136. Single Number.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+           if nums.count(i) == 1:
+               return i
+        
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-n=int(input())
-summ=0
-for i in range(n):
-    for j in range(i+1,n):
-        if i+j == n:
-            summ+=1
-print(summ)
+n=list(map(int,input().split()))
+x=[]
+for i in n:
+    if n.count(i) == 1:
+        x.append(i)
+print(x)
+
+        
             
