a04fb22375cf38fb1f3d63212858cfee7475eabd
gehad-ahme <gaded3082003@gmail.com>
2024-08-15 16:56:44 +0300

    project

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -5,3 +5,16 @@ class Solution:
            if nums.count(i) == 1:
                return i
         
+
+# other solution using stack runtime <
+
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        output=[]
+        for i in nums:
+            if i not in output:
+                output.append(i)
+            else:
+                output.remove(i)
+        return output.pop()
--- test.py
+++
@@ -1,9 +0,0 @@
-n=list(map(int,input().split()))
-x=[]
-for i in n:
-    if n.count(i) == 1:
-        x.append(i)
-print(x)
-
-        
-            
